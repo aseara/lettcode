cb4e3a1ebfc7fb3d8387fc02de0c87de529b76ec
qiujingde <qiujingde@hongguaninfo.com>
2017-04-17 09:11:21 +0800

    a500 + a557

---
+++ src/test/java/com/aseara/lettcode/a500/SolutionTest.java
@@ -0,0 +1,24 @@
+package com.aseara.lettcode.a500;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by qiujingde on 2017/4/16.
+ *
+ */
+public class SolutionTest {
+    @Test
+    public void findWords() throws Exception {
+
+        String[] input = new String[] {"Hello", "Alaska", "Dad", "Peace"};
+        String[] output = new String[] {"Alaska", "Dad"};
+
+        Solution solution = new Solution();
+
+        assertEquals(output, solution.findWords(input));
+
+    }
+
+}
---
+++ src/test/java/com/aseara/lettcode/a557/SolutionTest.java
@@ -0,0 +1,37 @@
+package com.aseara.lettcode.a557;
+
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by qiujingde on 2017/4/15.
+ * LettCode解题测试
+ */
+public class SolutionTest {
+
+    /**
+     * 解决方案。
+     */
+    private static Solution solution;
+
+    @BeforeClass
+    public static void beforeClass() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void reverseWords() {
+        String input = "Let's take LeetCode contest";
+        String output = "s'teL ekat edoCteeL tsetnoc";
+
+        assertEquals(output, solution.reverseWords(input));
+
+        input = "a b d ee$#% aef$ea eaef eaj ae##ea'fe";
+        output = "a b d %#$ee ae$fea feae jae ef'ae##ea";
+        assertEquals(output, solution.reverseWords(input));
+    }
+
+}
