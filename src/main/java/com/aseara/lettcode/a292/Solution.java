60d95a652545d8ff5ba33dcbbf1b477ef9d500c8
aseara <aseara@163.com>
2017-04-18 00:24:50 +0800

    a292 finished

--- src/main/java/com/aseara/lettcode/a292/Solution.java
+++ src/main/java/com/aseara/lettcode/a292/Solution.java
@@ -1,8 +1,5 @@
 package com.aseara.lettcode.a292;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Created by 邱境德 on 2017/4/17.
  * Algorithm 292
@@ -10,20 +7,7 @@ import java.util.Set;
 public class Solution {
 
     public boolean canWinNim(int n) {
-        if (n <= 3) {
-            return true;
-        }
-        if (n == 4) {
-            return false;
-        }
-        Set<Integer> fails = new HashSet<>();
-        fails.add(4);
-        for (int i = 5; i <= n; i++) {
-            if (!fails.contains(i - 1) && !fails.contains(i - 2) && !fails.contains(i - 3)) {
-                fails.add(i);
-            }
-        }
-        return !fails.contains(n);
+        return n % 4 > 0;
     }
 
 }
