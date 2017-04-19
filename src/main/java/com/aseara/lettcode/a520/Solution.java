5cc7c7a26bd8b6379bcd5ca6f9a486453ff68602
qiujingde <qiujingde@hongguaninfo.com>
2017-04-19 13:53:23 +0800

    a520

---
+++ src/main/java/com/aseara/lettcode/a520/Solution.java
@@ -0,0 +1,13 @@
+package com.aseara.lettcode.a520;
+
+/**
+ * Created by qiujingde on 2017/4/19.
+ * Algorithm 520
+ */
+public class Solution {
+
+    public boolean detectCapitalUse(String word) {
+        return word.matches("[A-Z]+|[a-z]+|[A-Z][a-z]*");
+    }
+
+}
