40566a18c59bdfa8299b3dad59b690d1e9f5db77
qiujingde <qiujingde@hongguaninfo.com>
2019-10-23 14:17:57 +0800

    242 提交

--- src/main/java/com/aseara/leetcode/editor/cn/a66/PlusOne.java
+++ src/main/java/com/aseara/leetcode/editor/cn/a66/PlusOne.java
@@ -60,10 +60,10 @@ class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] != 9) {
-                int[] result = new int[ digits.length ];
-                System.arraycopy(digits, 0, result, 0, i);
-                result[i] = digits[i] + 1;
-                return result;
+                digits[i] ++;
+                return digits;
+            } else {
+                digits[i] = 0;
             }
         }
         int[] result = new int[ digits.length + 1 ];
