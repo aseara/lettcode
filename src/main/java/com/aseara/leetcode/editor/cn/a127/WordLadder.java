aae832513e55f2a707bcbd1b56603989cf642869
aseara <aseara@163.com>
2019-11-01 00:20:23 +0800

    127.单词接龙 映射后BFS搜索

--- src/main/java/com/aseara/leetcode/editor/cn/a127/WordLadder.java
+++ src/main/java/com/aseara/leetcode/editor/cn/a127/WordLadder.java
@@ -44,8 +44,12 @@ package com.aseara.leetcode.editor.cn.a127;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -77,7 +81,11 @@ class WordLadder {
 class Solution {
 
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        List<String> checkList = new LinkedList<>();
+        return path2(beginWord, endWord, wordList);
+    }
+
+    private int path1(String beginWord, String endWord, List<String> wordList) {
+        LinkedList<String> checkList = new LinkedList<>();
         for (String w : wordList) {
             if (!w.equals(endWord)) {
                 checkList.add(w);
@@ -100,15 +108,15 @@ class Solution {
                 if (canGo(crt, endWord)) {
                     return step;
                 }
-                List<String> nextWords = new LinkedList<>();
-                for (String next : checkList) {
+                int checkSize = checkList.size();
+                for (int j = 0; j < checkSize; j++) {
+                    String next = checkList.poll();
                     if (canGo(crt, next)) {
                         queue.add(next);
                     } else {
-                        nextWords.add(next);
+                        checkList.add(next);
                     }
                 }
-                checkList = nextWords;
             }
         }
 
@@ -119,8 +127,66 @@ class Solution {
         int diff = 0;
         for (int i = 0; i < a.length(); i++) {
             diff += a.charAt(i) == b.charAt(i) ? 0 : 1;
+            if (diff > 1) {
+                return false;
+            }
         }
         return diff == 1;
     }
+
+    private int path2(String beginWord, String endWord, List<String> wordList) {
+        // abc -> *bc, a*c, ab*
+        Map<String, List<String>> map1 = new HashMap<>(wordList.size());
+        // *bc -> abc, bbc, cbc
+        Map<String, List<String>> map2 = new HashMap<>(wordList.size() * 3);
+
+        for(String word : wordList) {
+            fillMap(map1, map2, word);
+        }
+        fillMap(map1, map2, beginWord);
+
+        if (map1.get(endWord) == null) {
+            return 0;
+        }
+
+        // endWord -> *bc, a*c, ab*
+        Set<String> checkSet = new HashSet<>(map1.remove(endWord));
+
+        LinkedList<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+
+        int step = 1;
+        while (!queue.isEmpty() && !map1.isEmpty()) {
+            step++;
+            int batch = queue.size();
+            for (int i = 0; i < batch; i++) {
+                String crt = queue.poll();
+                List<String> paths = map1.remove(crt);
+                if (paths == null) {
+                    continue;
+                }
+                for (String path: paths) {
+                    if (checkSet.contains(path)) {
+                        return step;
+                    }
+                    for (String next : map2.get(path)) {
+                        if (map1.containsKey(next)) {
+                            queue.add(next);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    private void fillMap(Map<String, List<String>> map1, Map<String, List<String>> map2, String word) {
+        for (int i = 0; i < word.length(); i++) {
+            String pathWord = word.substring(0, i) + '*' + word.substring(i + 1);
+            map1.computeIfAbsent(word, k -> new LinkedList<>()).add(pathWord);
+            map2.computeIfAbsent(pathWord, k -> new LinkedList<>()).add(word);
+        }
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
