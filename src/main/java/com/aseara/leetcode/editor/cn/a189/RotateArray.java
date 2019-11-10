cca6bce1f4f81e3cf262b6696583d62f926f7231
aseara <aseara@163.com>
2019-11-10 14:32:38 +0800

    189.旋转数组

--- src/main/java/com/aseara/leetcode/editor/cn/a189/RotateArray.java
+++ src/main/java/com/aseara/leetcode/editor/cn/a189/RotateArray.java
@@ -29,6 +29,8 @@ package com.aseara.leetcode.editor.cn.a189;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 /**
  * desc: 189.旋转数组 <br />
  * Date: 2019/11/9 <br/>
@@ -40,7 +42,20 @@ class RotateArray {
     
     @Test
     void test1() {
-    
+        int[] nums = { 1,2,3,4,5,6,7 };
+        int[] expected = { 5,6,7,1,2,3,4 };
+
+        solution.rotate(nums, 3);
+        assertArrayEquals(expected, nums);
+
+        nums = new int[]{ -1,-100,3,99 };
+        expected = new int[]{ 3,99,-1,-100 };
+
+        solution.rotate(nums, 2);
+        assertArrayEquals(expected, nums);
+
+        nums = new int[]{ 1, 2 };
+        solution.rotate(nums, 3);
     }
     
 }
@@ -49,7 +64,58 @@ class RotateArray {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void rotate(int[] nums, int k) {
-        
+        rotate3(nums, k);
     }
+
+    private void rotate1(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            int temp = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                int temp2 = nums[j];
+                nums[j] = temp;
+                temp = temp2;
+            }
+        }
+    }
+
+    private void rotate2(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            int temp = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = temp;
+        }
+    }
+
+    private void rotate3(int[] nums, int k) {
+        int len = nums.length;
+        if (len == 1) {
+            return;
+        }
+        int moves = 0;
+        for (int i = 0; i < k; i++) {
+            // 已经移动所有的节点
+            if (moves == len) {
+                return;
+            }
+
+            int cur = i;
+            int temp = nums[i];
+            do {
+                // for i , shift pos i + k
+                int next = cur + k > len - 1 ? (cur + k) % len : cur + k;
+
+                int shift = nums[next];
+                nums[next] = temp;
+                temp = shift;
+                moves ++;
+
+                cur = next;
+            } while (cur > i);
+
+        }
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
