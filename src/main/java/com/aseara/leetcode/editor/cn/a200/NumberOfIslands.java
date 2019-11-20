57586f87724e46be04e140748bf4473719e35822
aseara <aseara@163.com>
2019-11-21 00:07:12 +0800

    200.岛屿数量 并查集实现

--- src/main/java/com/aseara/leetcode/editor/cn/a200/NumberOfIslands.java
+++ src/main/java/com/aseara/leetcode/editor/cn/a200/NumberOfIslands.java
@@ -63,6 +63,10 @@ class NumberOfIslands {
 class Solution {
 
     public int numIslands(char[][] grid) {
+        return solve2(grid);
+    }
+
+    private int solve1(char[][] grid) {
         int cnt = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
@@ -93,6 +97,87 @@ class Solution {
         if (y + 1 < grid[x].length && grid[x][y + 1] == '1') {
             destroyIsland(grid, x, y + 1);
         }
+    }
+
+    private int solve2(char[][] grid) {
+        if (grid == null || grid.length == 0
+                || grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        UnionFind unionFind = new UnionFind(m, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '0') {
+                    unionFind.count --;
+                } else {
+                    if (i + 1 < m && grid[i + 1][j] == '1') {
+                        unionFind.union(i, j, i + 1, j);
+                    }
+                    if (j + 1 < n && grid[i][j + 1] == '1') {
+                        unionFind.union(i, j, i, j + 1);
+                    }
+                }
+            }
+        }
+
+        return unionFind.count;
+    }
+
+    static class UnionFind {
+        int count;
+        int[] parent;
+        int[] rank;
+
+        int m;
+        int n;
+
+        UnionFind(int m, int n) {
+            this.m = m;
+            this.n = n;
+            count = m * n;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    parent[i * n + j] = i * n + j;
+                }
+            }
+        }
+
+        int get(int p) {
+            while (parent[p] != p) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+
+        int get(int i, int j) {
+            return get(i * n + j);
+        }
+
+        void union(int p1, int p2) {
+            p1 = get(p1);
+            p2 = get(p2);
+            if (p1 != p2) {
+                if (rank[p2] > rank[p1]) {
+                    parent[p1] = p2;
+                } else {
+                    parent[p2] = p1;
+                    rank[p1] += rank[p1] == rank[p2] ? 1 : 0;
+                }
+                count --;
+            }
+        }
+
+        void union(int i1, int j1, int i2, int j2) {
+            union(i1 * n + j1, i2 * n + j2);
+        }
+
     }
 
 }
