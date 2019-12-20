c98d9e04b22e63bb18a5b3880fbae0d5401829fd
aseara <aseara@163.com>
2019-12-20 14:38:22 +0800

    226.翻转二叉树

---
+++ src/main/java/com/aseara/leetcode/editor/cn/a226/InvertBinaryTree.java
@@ -0,0 +1,70 @@
+//翻转一棵二叉树。 
+//
+// 示例： 
+//
+// 输入： 
+//
+//      4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9 
+//
+// 输出： 
+//
+//      4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1 
+//
+// 备注: 
+//这个问题是受到 Max Howell 的 原问题 启发的 ： 
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。 
+// Related Topics 树
+package com.aseara.leetcode.editor.cn.a226;
+
+import com.aseara.leetcode.editor.cn.base.TreeNode;
+import org.junit.jupiter.api.Test;
+
+/**
+ * desc: 226.翻转二叉树 <br />
+ * Date: 2019/12/20 <br/>
+ *
+ * @author qiujingde
+ */
+class InvertBinaryTree {
+    private Solution solution = new Solution();
+    
+    @Test
+    void test1() {
+    
+    }
+    
+}
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root != null) {
+            invertTree(root.left);
+            invertTree(root.right);
+            TreeNode tmp = root.left;
+            root.left = root.right;
+            root.right = tmp;
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ src/main/java/com/aseara/leetcode/editor/cn/base/TreeNode.java
@@ -0,0 +1,14 @@
+package com.aseara.leetcode.editor.cn.base;
+
+/**
+ * desc: <br />
+ * Date: 2019/12/20 <br/>
+ *
+ * @author qiujingde
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
