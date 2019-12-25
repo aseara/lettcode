1f0afa75638d06460bd49b5a1c0f6b5c7b6c5f1f
aseara <aseara@163.com>
2019-12-25 17:00:17 +0800

    437.路径总和 III

--- src/main/java/com/aseara/leetcode/editor/cn/a437/PathSumIii.java
+++ src/main/java/com/aseara/leetcode/editor/cn/a437/PathSumIii.java
@@ -34,6 +34,9 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import static com.aseara.leetcode.editor.cn.base.TreeNode.fromArray;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * desc: 437.路径总和 III <br />
  * Date: 2019/12/25 <br/>
@@ -45,7 +48,11 @@ class PathSumIii {
     
     @Test
     void test1() {
+        TreeNode root1 = fromArray(10,5,-3,3,2,null,11,3,-2,null,1);
+        assertEquals(3, solution.pathSum(root1, 8));
 
+        TreeNode root2 = fromArray(5,4,8,11,null,13,4,7,2,null,null,5,1);
+        assertEquals(3, solution.pathSum(root2, 22));
     }
     
 }
@@ -66,25 +73,25 @@ class Solution {
         if (root == null) {
             return 0;
         }
-        List<Integer> subSums = Collections.singletonList(sum);
-        return pathSum(root, subSums, sum);
+        int[] nums = new int[1000];
+        return pathSum(root, sum, nums, 0);
     }
 
-    private int pathSum(TreeNode node, List<Integer> sums, int sum) {
+    private int pathSum(TreeNode node, int sum, int[] nums, int l) {
         if (node == null) {
             return 0;
         }
-        int result = 0;
-        List<Integer> subSums = new LinkedList<>();
-        for (int subSum : sums) {
-            if (subSum == node.val) {
+        int result = sum == node.val ? 1 : 0;
+        int tmp = 0;
+        for (int i = l - 1; i >= 0; i--) {
+            tmp += nums[i];
+            if (sum == tmp + node.val) {
                 result ++;
             }
-            subSums.add(subSum - node.val);
         }
-        subSums.add(sum);
-        result += pathSum(node.left, subSums, sum);
-        result += pathSum(node.right, subSums, sum);
+        nums[l] = node.val;
+        result += pathSum(node.left, sum, nums, l + 1);
+        result += pathSum(node.right, sum, nums, l + 1);
         return result;
     }
 
--- src/main/java/com/aseara/leetcode/editor/cn/base/TreeNode.java
+++ src/main/java/com/aseara/leetcode/editor/cn/base/TreeNode.java
@@ -1,5 +1,8 @@
 package com.aseara.leetcode.editor.cn.base;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
  * desc: <br />
  * Date: 2019/12/20 <br/>
@@ -11,4 +14,39 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
+
+    public static TreeNode fromArray(Integer... array) {
+        if (array == null || array.length == 0 || array[0] == null) {
+            return null;
+        }
+        TreeNode root = new TreeNode(array[0]);
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeNode node = root;
+        for (int i = 1; i < array.length; i++) {
+            TreeNode next = null;
+            if (array[i] != null) {
+                next = new TreeNode(array[i]);
+                queue.add(next);
+            }
+            if (i % 2 == 1) {
+              node.left = next;
+            } else  {
+                node.right = next;
+                queue.poll();
+                node = queue.peek();
+                if (node == null) {
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = fromArray(10,5,-3,3,2,null,11,3,-2,null,1);
+        System.out.println(root.val);
+    }
+
 }
